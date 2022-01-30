ad96a883bfe2cb620fdbf75d73cea3eec8f5a103
mabdullahsoyturk <msoyturk20@ku.edu.tr>
2022-01-30 20:23:28 +0300

    add GPU results

--- README.md
+++ README.md
@@ -6,9 +6,20 @@ To understand how it works, please refer to: https://nenadmarkus.com/p/fusing-ba
 
 I ran both implementations for each input size 100 times.
 
+Hardware: AMD Ryzen 9 5900HS
+
 | Input Size        | Normal           | Fused  | Speedup |
 | ------------- |:-------------:| -----:| -----:|
 | (16, 3, 32, 32)      | 0.034 | 0.026 | 1.322x|
 | (16, 3, 64, 64)      | 0.342     |   0.144 | 2.374x|
 | (16, 3, 128, 128) | 1.484     |    0.854 |  1.738x|
 | (16, 3, 256, 256) | 6.283     |    4.172 |  1.506x|
+
+Hardware: GeForce RTX 3060 Mobile
+
+| Input Size        | Normal           | Fused  | Speedup |
+| ------------- |:-------------:| -----:| -----:|
+| (16, 3, 32, 32)      | 0.542 | 0.005 | 111.341x|
+| (16, 3, 64, 64)      | 0.014     |   0.007 | 2.093x|
+| (16, 3, 128, 128) | 0.014     |    0.005 |  2.820x|
+| (16, 3, 256, 256) | 0.013     |    0.006 |  2.183x|
--- fusion.py
+++ fusion.py
@@ -10,10 +10,10 @@ def fuse_convolution_and_batchnorm(convolution, batchnorm):
         stride=convolution.stride,
         padding=convolution.padding,
         bias=True
-    )
+    ).cuda()
 
     # prepare filters
-    conv_weights = convolution.weight.clone().view(convolution.out_channels, -1)
+    conv_weights = convolution.weight.clone().view(convolution.out_channels, -1).cuda()
     batchnorm_weights = torch.diag(batchnorm.weight.div(torch.sqrt(batchnorm.eps + batchnorm.running_var)))
     updated_weight = torch.mm(batchnorm_weights, conv_weights).view(fused_convolution.weight.size())
     
@@ -21,7 +21,7 @@ def fuse_convolution_and_batchnorm(convolution, batchnorm):
     fused_convolution.weight.copy_(updated_weight)
 
     # prepare bias
-    conv_bias = torch.zeros(convolution.weight.size(0))
+    conv_bias = torch.zeros(convolution.weight.size(0)).cuda()
     batchnorm_bias = batchnorm.bias - batchnorm.weight.mul(batchnorm.running_mean).div(torch.sqrt(batchnorm.running_var + batchnorm.eps))
     updated_bias = torch.matmul(batchnorm_weights, conv_bias) + batchnorm_bias
 
@@ -31,41 +31,53 @@ def fuse_convolution_and_batchnorm(convolution, batchnorm):
     return fused_convolution
 
 def main():
+    torch.set_grad_enabled(False)
+
     input_sizes = [input_size for input_size in range(32, 257, 32)]
 
     rn18 = torchvision.models.resnet18(pretrained=True)
-    rn18.eval()
+    rn18.cuda()
 
     net = torch.nn.Sequential(
         rn18.conv1,
         rn18.bn1
     )
 
-    with torch.no_grad():
-        for size in input_sizes:
-            x = torch.randn(16, 3, size, size)
+    net.cuda()
 
-            # normal run
-            normal_time = 0
-            for _ in range(100):
-                start = time.time()
-                y1 = net.forward(x)
-                end = time.time()
-                normal_time += end - start
-            # print(f'Normal run took: {(normal_time / 100):.3f} seconds on average')
+    for size in input_sizes:
+        x = torch.randn(16, 3, size, size).cuda()
 
-            # fused run
-            fused_convolution = fuse_convolution_and_batchnorm(net[0], net[1])
+        # normal run
+        normal_time = 0
+        y1 = None
+        for iteration in range(100):
+            if iteration < 10:
+                continue
+            start = time.time()
+            y1 = net.forward(x)
+            end = time.time()
+            normal_time += end - start
+        # print(f'Normal run took: {(normal_time / 100):.3f} seconds on average')
 
-            fused_time = 0
-            for _ in range(100):
-                start = time.time()
-                y2 = fused_convolution.forward(x)
-                end = time.time()
-                fused_time += end - start
-            # print(f'Fused run took: {(fused_time / 100):.3f} seconds on average')
-            
-            print(f'For input (16, 3, {size}, {size}), normal time: {normal_time:.3f}, fused_time: {fused_time:.3f} speedup: {(normal_time / fused_time):.3f}x')
+        # fused run
+        fused_convolution = fuse_convolution_and_batchnorm(net[0], net[1])
+
+        fused_time = 0
+        y2 = None
+        for iteration in range(100):
+            if iteration < 10:
+                continue
+            start = time.time()
+            y2 = fused_convolution.forward(x)
+            end = time.time()
+            fused_time += end - start
+        # print(f'Fused run took: {(fused_time / 100):.3f} seconds on average')
+
+        d = (y1 - y2).norm().div(y1.norm()).item()
+        print("error: %.8f" % d)
+        
+        print(f'For input (16, 3, {size}, {size}), normal time: {normal_time:.3f}, fused_time: {fused_time:.3f} speedup: {(normal_time / fused_time):.3f}x')
 
 
 if __name__ == '__main__':
